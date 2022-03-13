d421225f42129da2db964d61bb02fb9dff126619
naens <naens@gmx.com>
2022-03-13 09:39:55 +0100

    improve clean.bat

--- clean.bat
+++ clean.bat
@@ -1,10 +1,13 @@
 @echo off
-del *.bak
-del *.com
-del *.sym
-del *.rel
-del *.z80
-del *.mac
-del *.ram
-del *.rom
-del *.lst
+del *.bak > NUL
+del *.com > NUL
+del *.sym > NUL
+del *.rel > NUL
+del *.z80 > NUL
+del *.mac > NUL
+del *.ram > NUL
+del *.rom > NUL
+del *.lst > NUL
+del *.obj > NUL
+del *.mp1 > NUL
+del *.86 > NUL
